d4b0e701aa52262efb99e3fa9426df534c75e9d7
前尘旧梦 <xiang@morthine.com>
2024-07-11 21:01:06 +0800

    [Feat] Add global defs

---
+++ include/mx/algo/mx_algo_defs.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file    mx_algo_defs.hpp
+ * @author  Morthine Xiang (xiang@morthine.com)
+ * @brief   mx-algo library definitions
+ * @version 1.0
+ * @date    2024-07-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MX_ALGO_DEFS_HPP
+#define MX_ALGO_DEFS_HPP
+
+#include <cstdint>
+
+#include <algorithm>
+#include <array>
+
+#endif // MX_ALGO_DEFS_HPP
---
+++ source/mx_algo.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file    mx_algo.cpp
+ * @author  Morthine Xiang (xiang@morthine.com)
+ * @brief   mx-algo library
+ * @version 1.0
+ * @date    2024-07-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
